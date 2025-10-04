23055766172983fada48915cebda015a15fa070a
SUMIT DHAMA <sumitdhama1512@gmail.com>
2025-10-04 16:19:05 +0530

    Uploaded frontened and dashboard

---
+++ frontened/src/landing_page/home/Award.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+function Awards() {
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-6 p-5 mt-4">
+          <img src="assests/largestBroker.svg" alt="largestBroker" style={{ width: "85%" }}/>
+        </div>
+        <div className="col-6 p-5 fs-7 mt-2">
+          <h2>Largest stock broker in India</h2>
+          <p className="mb-5">2+ million Zerodha clients contribute to over 15% of all retail order volumes in India daily by trading and investing in:
+          </p>
+          <div className="row">
+            <div className="col-6">
+              <ul>
+                <li>
+                  <p>Futures and Options</p>
+                </li>
+                <li>
+                  <p>Commodity derivatives</p>
+                </li>
+                <li>
+                  <p>Currency derivatives</p>
+                </li>
+              </ul>
+            </div>
+            <div className="col-6">
+              <ul>
+                <li>
+                  <p>Stocks & IPOs</p>
+                </li>
+                <li>
+                  <p>Direct mutual funds</p>
+                </li>
+                <li>
+                  <p>Bonds and Govt. Securities</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <img src="assests/pressLogos.png" alt="pressLogo" style={{ width: "90%" }} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Awards;
